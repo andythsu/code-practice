ffff80441fd86266a3f4a722767c269e89fe88ec
Su, TzuHsiang <tsu@netsuite.com>
2019-06-21 10:19:44 -0400

    commit

---
+++ src/CoinChange.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * TODO: Enter a paragraph that summarizes what the class does and why someone might want to utilize it
+ * <p>
+ * Copyright © 2019, Oracle and/or its affiliates. All rights reserved.
+ *
+ * @author tsu
+ * @since 2019-05-13
+ */
+class CoinChange {
+    public static class Coin {
+        int value;
+        int amount;
+
+        public Coin(int value, int amount) {
+            this.value = value;
+            this.amount = amount;
+        }
+
+        @Override
+        public String toString() {
+            return "value: " + this.value;
+        }
+    }
+
+    public static void main(String[] args) {
+        int target = 16;
+        System.out.println("target: " + target);
+
+        ArrayList<Coin> coins = new ArrayList<>(getArrayList());
+        // sort descending
+        coins.sort((Coin o1, Coin o2) -> o2.value - o1.value);
+        ArrayList<Coin> greedyResult = new ArrayList<>();
+        boolean isGreedyPossible = greedy(new ArrayList<>(coins), target, greedyResult);
+        System.out.println("is greedy possible: " + isGreedyPossible);
+        if (isGreedyPossible) System.out.println("greedy result: " + Arrays.toString(greedyResult.toArray()));
+
+        ArrayList<Coin> coins2 = new ArrayList<>(getArrayList());
+        // sort ascending
+        coins2.sort((Coin o1, Coin o2) -> o1.value - o2.value);
+        List<List<Coin>> backtrackResult = new ArrayList<>();
+        backtrack(coins2, target, backtrackResult, new ArrayList<>());
+        boolean isBackTrackPossible = backtrackResult.size() > 0;
+        System.out.println("is backtrack possible: " + isBackTrackPossible);
+        if (isBackTrackPossible) System.out.println("backtrack result: " + Arrays.toString(backtrackResult.toArray()));
+
+
+        ArrayList<Coin> coins3 = new ArrayList<>(getArrayList());
+        // sort ascending
+        coins3.sort((Coin o1, Coin o2) -> o1.value - o2.value);
+        System.out.println("number of ways by dp: " + dp(coins3, target));
+    }
+
+    public static int dp(List<Coin> coins, int target) {
+        int[] table = new int[target + 1];
+        Arrays.fill(table, 0);
+        // base case, when target is 0, we have 1 way of changing 0 coin
+        table[0] = 1;
+
+        for (int i = 0; i < coins.size(); i++) {
+            Coin currentCoin = coins.get(i);
+            for (int j = 0; j < table.length; j++) {
+                if (currentCoin.amount > 0 && currentCoin.value <= j) {
+                    int oldTableValue = table[j];
+                    table[j] = table[j] + table[j - currentCoin.value];
+                    // only update amount of coins when table is altered
+                    if (table[j] != oldTableValue) currentCoin.amount -= 1;
+                }
+            }
+        }
+        return table[target];
+    }
+
+    public static void backtrack(ArrayList<Coin> coins, int target, List<List<Coin>> result, ArrayList<Coin> temp) {
+        if (target < 0) return;
+        if (target == 0) result.add(new ArrayList<>(temp));
+        for (int i = 0; i < coins.size(); i++) {
+            Coin currentCoin = coins.get(i);
+            if (currentCoin.amount > 0) {
+                temp.add(currentCoin);
+                currentCoin.amount -= 1;
+                backtrack(coins, target - currentCoin.value, result, temp);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+    public static boolean greedy(ArrayList<Coin> coins, int target, ArrayList<Coin> result) {
+        if (coins == null) return false;
+
+        int coinIndex = 0;
+        while (target > 0 && coins.size() > coinIndex) {
+            Coin c = coins.get(coinIndex);
+            if (c.amount == 0 || c.value > target) {
+                coinIndex++;
+            } else {
+                target = target - c.value;
+                result.add(c);
+                c.amount = c.amount - 1;
+            }
+        }
+        return target == 0;
+    }
+
+    public static ArrayList<Coin> getArrayList() {
+        ArrayList<Coin> coins = new ArrayList<Coin>() {{
+            add(new Coin(1, 0));
+            add(new Coin(2, 3));
+            add(new Coin(5, 1));
+            add(new Coin(10, 10));
+        }};
+        return new ArrayList<>(coins);
+    }
+
+}
---
+++ src/TwoArray.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two array and a target, find one element from each array that will sum to closest to target
+ * <p>
+ * Copyright © 2019, Oracle and/or its affiliates. All rights reserved.
+ *
+ * @author tsu
+ * @since 2019-06-20
+ */
+public class TwoArray {
+    public static void main(String[] args){
+        int[] arr1 = new int[]{1,2,3,4,5};
+        int[] arr2 = new int[]{6,7,8,9,10};
+        int target = 17;
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        // iterate through arr1, find the remainder in arr2 using binary search
+        Map<Integer, Integer> result = new HashMap<>();
+        for(int i=0; i<arr1.length; i++){
+            int remainder = target - arr1[i];
+            int valueFoundInSecondArr = binarySearch(arr2, remainder);
+            result.put(arr1[i], valueFoundInSecondArr);
+        }
+
+        // loop through the map, find the closest to target
+        int min = Integer.MAX_VALUE;
+        int[] answer = new int[2];
+        for(Integer key : result.keySet()){
+            int value = result.get(key);
+            int total = key + value;
+            int absDiff = Math.abs(total - target);
+            if(absDiff <= min){
+                min = absDiff;
+                answer[0] = key;
+                answer[1] = value;
+            }
+        }
+        System.out.println("array 1: " + Arrays.toString(arr1));
+        System.out.println("array 2: " + Arrays.toString(arr2));
+        System.out.println("target: " + target);
+        System.out.println("closest to target: " + Arrays.toString(answer));
+    }
+    public static int binarySearch(int[] arr, int target){
+        int l=0, r = arr.length - 1;
+        while(l <= r){
+            int m = l + (r-l)/2;
+            if(arr[m] == target){
+                return arr[m];
+            }
+            if(arr[m] < target) {
+                l = m + 1;
+            }else{
+                r = m - 1;
+            }
+        }
+        // if we cannot find the needed value, find the closest using left and right
+        int leftValue = l >= arr.length ? arr[arr.length - 1] : arr[l];
+        int rightValue = r < 0 ? arr[0] : arr[r];
+        if(Math.abs(target - leftValue) > Math.abs(target - rightValue)){
+            return rightValue;
+        }else{
+            return leftValue;
+        }
+    }
+}
