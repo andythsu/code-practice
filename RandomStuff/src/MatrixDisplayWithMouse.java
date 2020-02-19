7fb65de237f976591891e7c384e42bfaaf622feb
Andy <f2280c@gmail.com>
2020-02-19 18:20:53 -0500

    reformatted

--- RandomStuff/src/MatrixDisplayWithMouse.java
+++ RandomStuff/src/MatrixDisplayWithMouse.java
@@ -1,10 +1,7 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Toolkit;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.event.MouseListener;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /* [MatrixDisplayWithMouse.java]
  * A small program showing how to use the MatrixDisplayWithMouse class
@@ -12,93 +9,82 @@ import java.awt.event.MouseEvent;
  * @author Mangat
  */
 
-
 class MatrixDisplayWithMouse extends JFrame {
-  
-  int maxX,maxY, GridToScreenRatio;
-  int[][] matrix;
-  
-  MatrixDisplayWithMouse(String title, int[][] matrix) {
-    super(title);
-    
-    this.matrix = matrix;
-    maxX = Toolkit.getDefaultToolkit().getScreenSize().width;
-    maxY = Toolkit.getDefaultToolkit().getScreenSize().height;
-    GridToScreenRatio = maxY / (matrix.length+1);  //ratio to fit in screen as square map
-    
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-    
-    this.add(new MatrixPanel());
-    
-    this.setVisible(true);
-  }
-  
-   public void refresh() { 
-    this.repaint();
-  }
-  
-  //Inner Class 
-  class MatrixPanel extends JPanel {
-    
-    MatrixPanel() { 
-      
-      addMouseListener(new MatrixPanelMouseListener());
-    }
-    
-    public void paintComponent(Graphics g) {        
-      super.repaint();
-      
-      setDoubleBuffered(true); 
-      g.setColor(Color.BLACK);
-      g.drawOval(50, 50, 50, 50);
 
-      
-       for(int i = 0; i<matrix[0].length;i=i+1)  { 
-        for(int j = 0; j<matrix.length;j=j+1)  { 
-          
-          if (matrix[i][j]==1)    //This block can be changed to match character-color pairs
-            g.setColor(Color.RED);
-          else if (matrix[i][j]==2)
-            g.setColor(Color.BLUE);
-          else 
-            g.setColor(Color.GREEN);
-          
-          g.fillRect(j*GridToScreenRatio, i*GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
-          g.setColor(Color.BLACK);
-          g.drawRect(j*GridToScreenRatio, i*GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
+    int maxX, maxY, GridToScreenRatio;
+    int[][] matrix;
+
+    MatrixDisplayWithMouse(String title, int[][] matrix) {
+        super(title);
+
+        this.matrix = matrix;
+        maxX = Toolkit.getDefaultToolkit().getScreenSize().width;
+        maxY = Toolkit.getDefaultToolkit().getScreenSize().height;
+        GridToScreenRatio = maxY / (matrix.length + 1);  //ratio to fit in screen as square map
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        this.add(new MatrixPanel());
+
+        this.setVisible(true);
+    }
+
+    public void refresh() {
+        this.repaint();
+    }
+
+    //Inner Class
+    class MatrixPanel extends JPanel {
+        MatrixPanel() {
+            addMouseListener(new MatrixPanelMouseListener());
         }
-      }
-      
-      
-    }
-  }
-  
-  
-  //Mouse Listener 
-  class MatrixPanelMouseListener implements MouseListener{ 
-     //Mouse Listner Stuff
-   public void mousePressed(MouseEvent e) {
-       System.out.println("Mouse pressed; # of clicks: " + e.getClickCount());
-       System.out.println("x: " + e.getPoint().x + ",y: " + e.getPoint().y);
+
+        public void paintComponent(Graphics g) {
+            super.repaint();
+
+            setDoubleBuffered(true);
+            g.setColor(Color.BLACK);
+            g.drawOval(50, 50, 50, 50);
+
+            for (int i = 0; i < matrix[0].length; i = i + 1) {
+                for (int j = 0; j < matrix.length; j = j + 1) {
+                    if (matrix[i][j] == 1)    //This block can be changed to match character-color pairs
+                        g.setColor(Color.RED);
+                    else if (matrix[i][j] == 2)
+                        g.setColor(Color.BLUE);
+                    else
+                        g.setColor(Color.GREEN);
+                    g.fillRect(j * GridToScreenRatio, i * GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j * GridToScreenRatio, i * GridToScreenRatio, GridToScreenRatio, GridToScreenRatio);
+                }
+            }
+        }
     }
 
-    public void mouseReleased(MouseEvent e) {
-      System.out.println("Mouse released; # of clicks: " + e.getClickCount());
-    }
+    //Mouse Listener
+    class MatrixPanelMouseListener implements MouseListener {
+        //Mouse Listner Stuff
+        public void mousePressed(MouseEvent e) {
+            System.out.println("Mouse pressed; # of clicks: " + e.getClickCount());
+            System.out.println("x: " + e.getPoint().x + ",y: " + e.getPoint().y);
+        }
 
-    public void mouseEntered(MouseEvent e) {
-       System.out.println("Mouse entered");
-    }
+        public void mouseReleased(MouseEvent e) {
+            System.out.println("Mouse released; # of clicks: " + e.getClickCount());
+        }
 
-    public void mouseExited(MouseEvent e) {
-       System.out.println("Mouse exited");
-    }
+        public void mouseEntered(MouseEvent e) {
+            System.out.println("Mouse entered");
+        }
 
-    public void mouseClicked(MouseEvent e) {
-       System.out.println("Mouse clicked (# of clicks: "+ e.getClickCount() + ")");
+        public void mouseExited(MouseEvent e) {
+            System.out.println("Mouse exited");
+        }
+
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("Mouse clicked (# of clicks: " + e.getClickCount() + ")");
+        }
     }
-  
-  }
-  
 }
